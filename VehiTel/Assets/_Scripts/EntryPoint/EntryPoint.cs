ae59de525d691120b33af96cffacbc3e59e59632
TrapTap277 <sway8318@gmail.com>
2024-09-26 23:52:16 +0300

    Added Entry Point

---
+++ VehiTel/Assets/_Scripts/EntryPoint/EntryPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using _Scripts.Car.Checker;
+using _Scripts.Car.Data;
+using _Scripts.TextWriters;
+using UnityEngine;
+
+namespace _Scripts.EntryPoint
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        [SerializeField] private DataWriter dataWriter;
+        [SerializeField] private CarData carData;
+
+        private readonly List<ICheck> _checks = new List<ICheck>();
+
+        private CarReader _carReader;
+
+        private void Start()
+        {
+            _checks.Add(new CheckEngineStatus(carData, dataWriter));
+            _checks.Add(new CheckTransmissionMode(carData, dataWriter));
+            _checks.Add(new FindNearestCar(carData, dataWriter));
+            _checks.Add(new SpeedChecker(carData, dataWriter));
+            _checks.Add(new CheckActiveGear(dataWriter));
+            _checks.Add(new RpmChecker(dataWriter));
+
+            _carReader = new CarReader(_checks);
+        }
+
+        private void Update()
+        {
+            Check();
+        }
+
+        private void Check()
+        {
+            _carReader.Check();
+        }
+    }
+}
