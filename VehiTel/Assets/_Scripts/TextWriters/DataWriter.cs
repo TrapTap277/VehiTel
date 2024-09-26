037b5998e684e2998bfe692d68227b58b6428218
TrapTap277 <sway8318@gmail.com>
2024-09-26 23:51:59 +0300

    Added Writer Logic

---
+++ VehiTel/Assets/_Scripts/TextWriters/DataWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace _Scripts.TextWriters
+{
+    public class DataWriter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI engineStatusText;
+        [SerializeField] private TextMeshProUGUI checkTransmissionMode;
+        [SerializeField] private TextMeshProUGUI findNearestCar;
+        [SerializeField] private TextMeshProUGUI speedChecker;
+        [SerializeField] private TextMeshProUGUI checkActiveGear;
+        [SerializeField] private TextMeshProUGUI rpmChecker;
+
+        private TextMeshProUGUI _currentText;
+
+        public void SetText(DataType dataType)
+        {
+            _currentText = dataType switch
+            {
+                DataType.EngineStatus => engineStatusText,
+                DataType.CheckTransmissionMode => checkTransmissionMode,
+                DataType.FindNearestCar => findNearestCar,
+                DataType.SpeedChecker => speedChecker,
+                DataType.CheckActiveGear => checkActiveGear,
+                DataType.RpmChecker => rpmChecker,
+                _ => throw new ArgumentOutOfRangeException(nameof(dataType), dataType, null)
+            };
+        }
+
+        public void Write(string value)
+        {
+            _currentText.text = value;
+        }
+    }
+}
---
+++ VehiTel/Assets/_Scripts/TextWriters/Writer.cs
@@ -0,0 +1,13 @@
+﻿namespace _Scripts.TextWriters
+{
+    public abstract class Writer
+    {
+        protected DataWriter DataWriter;
+
+        protected void Write(DataType dataType, string text)
+        {
+            DataWriter.SetText(dataType);
+            DataWriter.Write(text);
+        }
+    }
+}
